d111f8e464a10d77b1c2334b8d3d9dc143ab9858
Simona Meiler <simona.meiler@usys.ethz.ch>
2025-10-24 08:09:30 -0700

    update make exposure file

--- scripts/make_NA_exposure.py
+++ scripts/make_NA_exposure.py
@@ -12,7 +12,7 @@ from modules.exposure_utils import load_exposure_from_csv
 #exp_dir = Path("/home/groups/bakerjw/smeiler/climada_data/data/exposure")
 
 data_dir = Path('/Users/simonameiler/Documents/work/03_code/repos/hurricane_recovery_potential/data')
-exp_dir = data_dir / 'exposure'
+exp_dir = data_dir / 'exposure' / 'NA_coast_exposure'
 county_fp = data_dir / 'US_counties.shp'
 
 # Load data
